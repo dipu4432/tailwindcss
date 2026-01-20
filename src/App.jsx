f73b16c9d24e0e151769a756aff4e59b6bfe4e3d
Dipu Kumar <dipukumarpojhi@gmail.com>
2026-01-20 22:53:16 +0530

    Acme Rockets

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,7 @@ import { useState } from "react";
 function App() {
   return (
     <>
-      <div className="min-h-screen grid place-content-center radial-blue">
-          <div className="bg-emerald-500 w-52 h-52 rounded-full shadow-2xl grid place-content-center">
-            <div className="bg-teal-300 w-32 h-32 rounded-full grid place-content-center">
-              <div className="bg-green-300 w-16 h-16 rounded-full"></div>
-            </div>
-          </div>
-        </div>
+      
     </>
   );
 }
