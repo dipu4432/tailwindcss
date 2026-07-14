7100f536ded86c0159727b48257dbfed76e4e775
Dipu Kumar <dipukumarpojhi@gmail.com>
2026-07-14 11:43:20 +0530

    text, bg, button, padding, and margin done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 function App() {
-
   return (
-    <>
-      <h1>Welcome to Vite + React + Tailwind CSS</h1>
-    </>
-  )
+    <div className="min-h-screen p-5 bg-fuchsia-950 space-y-5">
+      <h1 className="text-3xl text-white font-bold">
+        Welcome to Vite + React + Tailwind CSS
+      </h1>
+      <p className="text-yellow-500 bg-gray-500 border-[2px] border-red-500 p-5 rounded-lg">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        dolores delectus animi illo sed, neque aperiam ipsum quae non
+        reprehenderit dolorem quisquam officia voluptas dolore porro voluptate
+        hic placeat perspiciatis?
+      </p>
+      <a
+        href="https://www.google.com"
+        className="inline-block bg-green-500 hover:bg-green-700 text-white py-3 px-7 rounded-full"
+      >
+        Click Here
+      </a>
+    </div>
+  );
 }
 
-export default App
+export default App;
